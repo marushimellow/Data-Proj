bb14d56e1aca80725941f25dc6e9e51aaf0b1775
marushimellow <57819442+marushimellow@users.noreply.github.com>
2020-12-05 13:20:53 +0800

    Update Studentupdate.java

--- Studentupdate.java
+++ Studentupdate.java
@@ -37,7 +37,7 @@ public class Studentupdate extends Studentdelete {
 						slot7 = s.next();
 						System.out.print("Enter New Birthday: ");
 						slot8 = s.next();
-						System.out.print("Enter New Address: ");
+						System.out.print("Enter New City: ");
 						slot4 = s.next();
 						System.out.print("Enter New Teacher: ");
 						slot3 = s.next();
